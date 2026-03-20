0fdbe4be89a12794390b30234c7e855cf4e2737c
akash more <akashmore411028@gamil.com>
2026-03-20 11:07:15 +0530

    backend changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
+import { json } from "express";
 
 export const  signup  = async (req,res)=>{
    const {fullName,password,email} =req.body;
@@ -93,5 +95,31 @@ export const logout = async (req,res)=>{
 }
 
 export const updateProfile = async (req,res)=>{
-    
+ try {
+    const userId = req.user._id;
+    const {profilePic} = req.body;
+    if(!profilePic){
+        return res.status(401).json({message:"profile pic is required"});
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    if(!uploadResponse){
+        return res.status(401).json({message:"issue while uploading profile pic"})
+    }
+    const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+
+    res.status(200).json(updatedUser);
+ } catch (error) {
+    console.log("error in update profile:",error.message);
+    return res.status(500).json({message:"Interal Server Error!"})
+ }
+}
+
+
+export const checkAuth = (req,res) =>{
+try {
+    return res.status(200).json(req.user);
+} catch (error) {
+   console.log("Error in checkAuth controller", error.message);
+   res.status(500).json({message:"Internal server error "})
+}
 }
